7a7f8157b20f650a9e39a1f319fdc846708ce825
Kambiz Moradi <kambizmoradi2009@gmail.com>
2023-12-23 13:24:47 +0330

    add command: NexusUp, NexusDown

--- X11/X11_setting_display.sh
+++ X11/X11_setting_display.sh
@@ -4,5 +4,5 @@
 xrandr --output DisplayPort-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --primary --mode 1920x1080 --pos 1920x0 --rotate normal
 
 
-xrandr --output DisplayPort-0 --brightness 0.85
+xrandr --output DisplayPort-0 --brightness 0.75
 xrandr --output HDMI-A-0 --brightness 0.85
